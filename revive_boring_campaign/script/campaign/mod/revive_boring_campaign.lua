78184a25d1bf8cf835af30c913906cf0c6b5f286
Arthur LAMBERT <arthur.lambert@soundskrit.ca>
2026-06-06 13:06:58 -0400

    update version

--- revive_boring_campaign/script/campaign/mod/revive_boring_campaign.lua
+++ revive_boring_campaign/script/campaign/mod/revive_boring_campaign.lua
@@ -10,7 +10,7 @@
 -- Module definition
 revive_boring_campaign = {
     name = "revive_boring_campaign",
-    log_prefix = "[RBC_DEBUG][campaign][v1.3.0]",
+    log_prefix = "[RBC_DEBUG][campaign][v1.3.1]",
 
     -- State tracking (will be saved)
     settings = {
